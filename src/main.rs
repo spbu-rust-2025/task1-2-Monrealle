8f108889208835861e78d0a5a2209ddf16db5373
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-27 02:38:06 +0300

    Realized amount with limiter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut sum = 0;
+    let mut invalid_input = false; // Было: ivalid_input (опечатка)
+
+    loop {
+        let mut input = String::new(); // Было: lt mup input (опечатки)
+
+        if io::stdin().read_line(&mut input).is_err() {
+            invalid_input = true;
+            continue;
+        }
+
+        let input = input.trim();
+
+        if input == "-1" {
+            break;
+        }
+
+        match input.parse::<i64>() {
+            Ok(num) => {
+                if num > 0 {
+                    sum += num;
+                } else {
+                    invalid_input = true;
+                }
+            }
+            Err(_) => {
+                invalid_input = true;
+            }
+        }
+    }
+
+    if invalid_input {
+        println!("NaN");
+    } else {
+        println!("{}", sum);
+    }
 }
